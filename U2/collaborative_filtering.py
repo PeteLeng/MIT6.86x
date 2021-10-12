36d357bf8e40dfc0df8a118f1b598d73cd40f496
PeteLeng <pete_leng@126.com>
2021-10-12 12:22:32 +0800

    Refactor collaborative filtering
    - rename function to factorization
    - rewrite the logic
        - introduced mutiple, factor matrix instead of row, column matrix
        - allow as input either a column (product) matrix or a row (user) matrix
    - add wrapper for factoring multiple times
    - add docstring

--- U2/collaborative_filtering.py
+++ U2/collaborative_filtering.py
@@ -12,31 +12,84 @@ import sympy as sp
 # - differentiate, solve for 0
 
 
-def collab_filter(n_row, n_col, rank, reg_coeff, real_y, fix_m, fix='col'):
-    if fix == 'col':
-        row_matrix = symbolic_matrix(n_row, rank)
-        col_matrix = fix_m
-        proj_y = np.matmul(row_matrix, col_matrix)
-        observed = ~np.isnan(real_y)
-        squared_errors = np.sum((proj_y[observed] - real_y[observed])**2)/2
-        regularization = np.sum(row_matrix**2) * reg_coeff/2
-        obj = squared_errors + regularization
-        best_row_matrix = np.empty([n_row, rank]).astype('object')
-        print(best_row_matrix)
-        for entry in row_matrix.flat:
-            i, j = int(entry.name[-2]), int(entry.name[-1])
-            print(i, j)
-            gradient = sp.diff(obj, entry)
-            best_row_matrix[i, j] = sp.solveset(gradient, entry)
-
-    # else:
-    #     row_matrix = np.ones(n_row, rank)
-    #     col_matrix = symbolic_matrix([rank, n_col])
-
-    return best_row_matrix
+def iterative_factorization(observed_matrix, reg_coeff, num_iterations, **kwargs):
+    factor, multiple = factorization(observed_matrix, reg_coeff, **kwargs)
+    # print('Given factor:\n', multiple, '\n', 'Estimated:\n', factor)
+    for i in range(num_iterations):
+        factor, multiple = factorization(observed_matrix, reg_coeff, factor_matrix=factor)
+        # print('Given factor:\n', multiple, '\n', 'Estimated:\n', factor)
+    if multiple.shape[1] == factor.shape[0]:
+        return np.matmul(multiple, factor)
+    else:
+        return np.matmul(factor, multiple)
 
 
-def symbolic_matrix(n_row, n_col, name='m'):
+def factorization(observed_matrix, reg_coeff, factor_matrix=None, rank=None, direction=None):
+    """
+    Given a observed matrix, its assumed rank, an initialed factor (optional),
+    find the multiple that optimized the objective function,
+    consisting of squared errors and regularization term.
+    :param observed_matrix: np.matrix
+        The user-product matrix
+    :param reg_coeff: int
+        Regularization terms
+    :param factor_matrix: np.matrix
+        Initialized factor matrix
+    :param rank: int
+        Assumed rank of the user-product matrix
+    :param direction: bool
+        Decide which matrix to initialize, 1 for product matrix, 0 for user matrix
+    :return: tuple of np.matrix
+
+    """
+    n_row, n_col = observed_matrix.shape
+    if factor_matrix is not None:
+        factor_row, factor_col = factor_matrix.shape
+        if factor_row in (n_row, n_col):
+            direction = 0
+            multiple_matrix = symbolic_matrix([factor_col, n_col])
+        elif factor_col in (n_row, n_col):
+            direction = 1
+            multiple_matrix = symbolic_matrix([n_row, factor_row])
+        else:
+            print('Dimension of factor matrix does not fit observed matrix')
+    elif rank is not None and direction is not None:
+        if direction:
+            factor_matrix = np.ones([rank, n_col])
+            multiple_matrix = symbolic_matrix([n_row, rank])
+        else:
+            factor_matrix = np.ones([n_row, rank])
+            multiple_matrix = symbolic_matrix([rank, n_col])
+    else:
+        raise ValueError('Provide either factor_matrix or rank and direction')
+
+    if direction:
+        est_matrix = np.matmul(multiple_matrix, factor_matrix)
+    else:
+        est_matrix = np.matmul(factor_matrix, multiple_matrix)
+    observed = ~np.isnan(observed_matrix)
+    squared_errors = np.sum((est_matrix[observed] - observed_matrix[observed])**2)/2
+    regularization = np.sum(multiple_matrix**2) * reg_coeff/2
+    obj = squared_errors + regularization
+    est_multiple_matrix = np.empty_like(multiple_matrix).astype('object')
+    for entry in multiple_matrix.flat:
+        i, j = int(entry.name[-2]), int(entry.name[-1])
+        gradient = sp.diff(obj, entry)
+        est_multiple_matrix[i, j] = sp.solveset(gradient, entry).args[0]
+
+    return est_multiple_matrix, factor_matrix
+
+
+def symbolic_matrix(dimension, name='m'):
+    """
+    Given the dimension, populate a matrix of sympy variables
+    :param dimension: list
+        list of rows and columns
+    :param name: str
+        name of the matrix
+    :return:
+    """
+    [n_row, n_col] = dimension
     m = np.empty([n_row, n_col]).astype('object')
     # m[:] = np.NaN
     for row in range(n_row):
@@ -53,8 +106,19 @@ if __name__ == '__main__':
         [2, np.NaN, 5]
     ])
     lmda = sp.Symbol('lmda')
-    print(collab_filter(2, 3, 1, lmda, examples, v))
 
     # Test symbolic matrix
     # ar1 = symbolic_matrix(3, 3, 'u')
     # print(ar1)
+
+    # Test factorization
+    res = factorization(examples, 1, factor_matrix=v)
+    # print(res)
+
+    # Test iterative factorization
+    res = iterative_factorization(examples, 1, 5, factor_matrix=v)
+    print(res)
+
+    # Debug
+    # product = np.array([[0.637439110697700, 3.05324815909688, 1.99458569294239]])
+    # print(factorization(examples, 1, factor_matrix=product))
